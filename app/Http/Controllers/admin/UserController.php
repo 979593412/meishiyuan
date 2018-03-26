52b553b27b28fb6faa6db8120c9d67446ec83362
jishijia <1826144263@qq.com>
2018-03-26 20:24:25 +0800

    季世甲上传(20.24)

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\Admin_User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Crypt; 
+use Illuminate\Support\Facades\Crypt;  
 
 use DB;
 
