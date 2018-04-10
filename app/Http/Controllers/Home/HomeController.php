acd8f4cfcd157b3685494c02052cefaa5b04d6fa
hasai11 <347501534@qq.com>
2018-04-10 12:48:23 +0800

    bug

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -31,7 +31,7 @@ class HomeController extends CommonController
 
         }
 
-        $lunbo=DB::table('Carousel')->get();
+        $lunbo=DB::table('carousel')->get();
         //左侧菜单栏分类
         $cates = Cate::get();
         $cates = $this->getTree($cates,0);
