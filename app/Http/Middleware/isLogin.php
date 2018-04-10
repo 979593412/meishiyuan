1c1b384601f34a21672f9fed5f1ce8bf61ee6ff0
979593412 <979593412@qq.com>
2018-04-10 11:42:48 +0800

    更新验证

--- app/Http/Middleware/isLogin.php
+++ app/Http/Middleware/isLogin.php
@@ -18,7 +18,7 @@ class isLogin
          if(session()->get('admin_user')){
              return $next($request);
          }else{
-             return redirect('admin/logins')->with('errors','请注意一下素质');
+             return redirect('admin/logins')->with('errors','请先登录');
          }
 
     }
